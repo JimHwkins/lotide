417d927f374359b7b938c770d3811185d0a810b7
JimHwkins <howlarrior@gmail.com>
2023-01-09 20:00:08 -0300

    Initial commit

---
+++ tail.js
@@ -0,0 +1,15 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = (arr) => {
+  return arr.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
